609f699f4b78f7bc128a719bb53009763327ba12
Reet <reeturani9032@gmail.com>
2026-07-03 10:10:11 +0530

    Fix ReferenceError by importing useEffect in submit page

--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import Link from 'next/link';
 import { addSubmission } from '@/lib/store';
 import './submit.css';
